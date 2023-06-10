afac97a9a237da9cf88a9873d7748c9025de851a
aahmedimran <ahmed.eng2709@gmail.com>
2023-06-11 00:02:30 +0500

    add react select

--- src/pages/accountBook.tsx
+++ src/pages/accountBook.tsx
@@ -1,13 +1,37 @@
 import Layout from '@/components/Layout'
-import React from 'react'
+import React, { useState } from 'react'
+import Select from 'react-select';
 
 const accountBook = () => {
+  const [dropDown, setDropdown] = useState<string[]>()
+
+  type optionsType = {
+    value: string;
+    label: string;
+  };
+  const options: optionsType[] = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
+  const handleChange = (dropDown: any) => {
+    setDropdown(dropDown)
+
+    console.log(dropDown,"dropDown");
+
+  }
+
   return (
     <Layout>
+      <div>
+        <Select
+          value={dropDown}
+          onChange={handleChange}
+          options={options}
+        />
 
-    <div>
-      accountBook
-    </div>
+
+      </div>
     </Layout>
   )
 }
